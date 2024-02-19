6bae239fb1555426b36348726aaf129ad9ed643a
芙宁娜 <141395963+KOKOMI12345@users.noreply.github.com>
2024-02-20 01:04:05 +0800

    Add files via upload

--- Log4p/Log4p.py
+++ Log4p/Log4p.py
@@ -1,19 +1,25 @@
 import logging
 import colorlog
 import os
-from Log4p.websocketHander import WebsocketHandler
+from websocketHander import WebsocketHandler
 import asyncio
-from Log4p.HttpHander import HTTPhandler , AsyncHTTPhandler
+from logging.handlers import QueueHandler, QueueListener
+from HttpHander import HTTPhandler , AsyncHTTPhandler
+
 class LogManager:
     def __init__(self) -> None:
-        pass
+        self.public_formatter = logging.Formatter(
+            fmt='%(asctime)s.%(msecs)03d | %(levelname)-8s | %(filename)s:%(lineno)d | %(name)s | %(funcName)s | %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
 
     def GetLogger(self, log_name: str = "default",
                   out_to_console: bool = True,
                   web_log_mode: bool = False,
                   WSpost_url: str = "",
                   HTTPpost_url: str = "",
-                  http_mode: bool = False):
+                  http_mode: bool = False,
+                  custom_formatter: logging.Formatter = None):
         # 确保日志名称有效
         log_name = log_name if log_name else "default"
         if out_to_console:
@@ -41,33 +47,41 @@ class LogManager:
                 datefmt='%Y-%m-%d %H:%M:%S',
                 log_colors=log_color_config
             )
-            console_handler.setFormatter(console_formatter)
+            if custom_formatter:
+                console_formatter = custom_formatter
+
+            if isinstance(console_handler, logging.StreamHandler):
+                console_formatter = colorlog.ColoredFormatter(fmt=f"%(log_color)s {console_formatter._fmt} %(reset)s",datefmt=console_formatter.datefmt, log_colors=log_color_config)
+                console_handler.setFormatter(console_formatter)
 
             logger.setLevel(logging.DEBUG)
             logger.addHandler(console_handler)
 
+        
         file_handler = logging.FileHandler(
             filename=f'logs/{log_name}/{log_name}.log', mode='a', encoding='utf-8')
         file_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter(
-            fmt='%(asctime)s.%(msecs)03d | %(levelname)-8s | %(filename)s:%(lineno)d | %(name)s | %(funcName)s | %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-        )
-        file_handler.setFormatter(formatter)
+        file_handler.setFormatter(self.public_formatter)
+
+        if custom_formatter:
+            file_handler.setFormatter(custom_formatter)
 
         # 检查代码是否在异步环境中运行
         if asyncio.iscoroutinefunction(logging.Handler.emit):
-            logger.addHandler(file_handler)
+            queue = asyncio.Queue()
+            queue_handler = QueueHandler(queue)
+            queue_listener = QueueListener(queue, file_handler)
+            logger.addHandler(queue_handler)
+            asyncio.ensure_future(queue_listener.start())
         else:
             logger.addHandler(file_handler)
 
         if web_log_mode and WSpost_url:
             websocket_handler = WebsocketHandler(WSpost_url)
             websocket_handler.setLevel(logging.INFO)
-            formatter = logging.Formatter(
-                fmt='%(asctime)s.%(msecs)03d | %(levelname)-8s | %(filename)s:%(lineno)d | %(name)s | %(funcName)s | %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S'
-            )
+            formatter = self.public_formatter
+            if custom_formatter:
+                formatter = custom_formatter
             websocket_handler.setFormatter(formatter)
             logger.addHandler(websocket_handler)
 
@@ -76,18 +90,16 @@ class LogManager:
             if asyncio.iscoroutinefunction(logging.Handler.emit):
                 async_http_hander = AsyncHTTPhandler(HTTPpost_url)
                 async_http_hander.setLevel(logging.INFO)
-                formatter = logging.Formatter(
-                    fmt='%(asctime)s.%(msecs)03d | %(levelname)-8s | %(filename)s:%(lineno)d | %(name)s | %(funcName)s | %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S'
-                )
+                formatter = self.public_formatter
+                if custom_formatter:
+                    formatter = custom_formatter
                 async_http_hander.setFormatter(formatter)
                 logger.addHandler(async_http_hander)
             http_handler = HTTPhandler(HTTPpost_url)
             http_handler.setLevel(logging.INFO)
-            formatter = logging.Formatter(
-                fmt='%(asctime)s.%(msecs)03d | %(levelname)-8s | %(filename)s:%(lineno)d | %(name)s | %(funcName)s | %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S'
-            )
+            formatter = self.public_formatter
+            if custom_formatter:
+                formatter = custom_formatter
             http_handler.setFormatter(formatter)
             logger.addHandler(http_handler)
 
